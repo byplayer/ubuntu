36ba938b4dec73c2620180f417087cea601982c5
byplayer <byplayer100@gmail.com>
2017-10-31 09:18:11 +0800

    add lubuntu

---
+++ script/lubuntu.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [[ ! "$LUBUNTU" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
+  exit
+fi
+
+SSH_USER=${SSH_USERNAME:-vagrant}
+
+apt -y install ssh-import-id g++ zlib1g-dev \
+  lubuntu-desktop gnome-terminal \
+  gettext tcl tk libsvn-perl asciidoc libcurl4-openssl-dev curl\
+  libexpat-dev zlib1g-dev libbz2-dev \
+  aptitude zsh autotools-dev automake libtool cmake \
+  libevent-dev libncurses5-dev zlib1g-dev libbz2-dev libyaml-dev \
+  libxml2-dev libxslt1-dev libreadline-dev xclip patch \
+  libx11-dev \
+  libgmp3-dev \
+  chromium-browser \
+  wget ncftp ntp \
+  ibus-mozc mozc-server mozc-utils-gui \
+  exuberant-ctags \
+  emacs24 emacsen-common rdtool-elisp emacs-goodies-el \
+  emacs-mozc \
+  markdown \
+  xfonts-shinonome python-xlib \
+  libterm-readkey-perl \
+  mlocate ssh \
+  libsqlite3-dev sqlite3 \
+  expect dkms \
+  libmcrypt-dev \
+  libffi-dev \
+  eblook lookup-el \
+  python-sphinx python-pip \
+  python-virtualenv \
+  python3-sphinx python3-pip \
+  python3-virtualenv \
+  postgresql postgresql-client libpq-dev \
+  manpages-dev \
+  iftop bmon iptraf nload nethogs speedometer slurm \
+  iptraf tcptrack vnstat  bwm-ng cbm pktstat netdiag \
+  ifstat nmon saidar \
+  jq whois \
+  nginx \
+  sam2p tokyotyrant tokyocabinet-bin libtokyotyrant-dev \
+  postfix \
+  cmigemo \
+  mailutils \
+  curl unzip libsnappy-dev \
+  qt4-qmake libqt4-dev xvfb \
+  rlwrap \
+  meld \
+  texi2html texinfo \
+  libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386 \
+  qemu-kvm virt-manager libvirt-bin bridge-utils \
+  libgdbm-dev libgroonga-dev
